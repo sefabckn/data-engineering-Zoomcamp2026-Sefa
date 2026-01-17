db5473f50961a6d2b37d8c2d992fb40489e03bdf
sefabckn <sefabckn@gmail.com>
2026-01-17 23:59:17 +0100

    Dockerfile updated, pg-network created, CLI paramaeters automatized with click, connected Pgadmin

--- 01-docker-terraform/pipeline/ingest_data.py
+++ 01-docker-terraform/pipeline/ingest_data.py
@@ -8,6 +8,7 @@ from sqlalchemy import create_engine
 
 from tqdm.auto import tqdm
 
+import click
 
 
 dtype = {
@@ -35,25 +36,25 @@ parse_dates = [
     "tpep_dropoff_datetime"
 ]
 
+## those CLI commands are for automatization the parameters instead of hardcoding the parameters
 
-def run():
-    year = 2021
-    month = 1
+@click.command()
+@click.option('--pg-user', default='root', help='PostgreSQL username')
+@click.option('--pg-pass', default='root', help='PostgreSQL password')
+@click.option('--pg-host', default='localhost', help='PostgreSQL host')
+@click.option('--pg-port', default='5434', help='PostgreSQL port')
+@click.option('--pg-db', default='ny_taxi', help='PostgreSQL database name')
+@click.option('--year', default=2021, type=int, help='Year of the data')
+@click.option('--month', default=1, type=int, help='Month of the data')
+@click.option('--chunksize', default=100000, type=int, help='Chunk size for ingestion')
+@click.option('--target-table', default='yellow_taxi_data', help='Target table name')
 
-    pg_user = 'root'
-    pg_passwd = 'root'
-    pg_host = 'localhost'
-    pg_port = 5434
-    pg_db = 'ny_taxi'
-
-    target_table = 'ny_yellow_taxi_data'
-    chunksize = 100_000
-
+def main(pg_user, pg_pass, pg_host, pg_port, pg_db, year, month, chunksize, target_table):
     prefix = 'https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow/'
     url = f'{prefix}/yellow_tripdata_{year}-{month:02d}.csv.gz'
 
     engine = create_engine(
-        f'postgresql://{pg_user}:{pg_passwd}@{pg_host}:{pg_port}/{pg_db}'
+        f'postgresql://{pg_user}:{pg_pass}@{pg_host}:{pg_port}/{pg_db}'
     )
 
     df_iter = pd.read_csv(
@@ -84,4 +85,4 @@ def run():
 
 
 if __name__ == '__main__':
-    run()
+    main()
