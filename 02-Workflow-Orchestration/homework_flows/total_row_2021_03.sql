ead01000234ce465120a813ef6945ef9c37e68c4
sefabckn <sefabckn@gmail.com>
2026-02-03 23:59:47 +0100

    Added homework with screenshots

---
+++ 02-Workflow-Orchestration/homework_flows/total_row_2021_03.sql
@@ -0,0 +1,2 @@
+SELECT count(1) as total_rows
+FROM `taxi-rides-ny-485508`.`zoomcamp`.`yellow_tripdata_2021_03`
---
+++ 02-Workflow-Orchestration/homework_flows/yellow_total_rows_2020.sql
@@ -0,0 +1,63 @@
+
+SELECT
+  COUNT(*) AS total_rows
+FROM (
+  SELECT
+    unique_row_id
+  FROM
+    `taxi-rides-ny-485508`.`zoomcamp`.`yellow_tripdata_2020_01`
+  UNION ALL
+  SELECT
+    unique_row_id
+  FROM
+    `taxi-rides-ny-485508`.`zoomcamp`.`yellow_tripdata_2020_02`
+  UNION ALL
+  SELECT
+    unique_row_id
+  FROM
+    `taxi-rides-ny-485508`.`zoomcamp`.`yellow_tripdata_2020_03`
+  UNION ALL
+  SELECT
+    unique_row_id
+  FROM
+    `taxi-rides-ny-485508`.`zoomcamp`.`yellow_tripdata_2020_04`
+  UNION ALL
+  SELECT
+    unique_row_id
+  FROM
+    `taxi-rides-ny-485508`.`zoomcamp`.`yellow_tripdata_2020_05`
+  UNION ALL
+  SELECT
+    unique_row_id
+  FROM
+    `taxi-rides-ny-485508`.`zoomcamp`.`yellow_tripdata_2020_06`
+  UNION ALL
+  SELECT
+    unique_row_id
+  FROM
+    `taxi-rides-ny-485508`.`zoomcamp`.`yellow_tripdata_2020_07`
+  UNION ALL
+  SELECT
+    unique_row_id
+  FROM
+    `taxi-rides-ny-485508`.`zoomcamp`.`yellow_tripdata_2020_08`
+  UNION ALL
+  SELECT
+    unique_row_id
+  FROM
+    `taxi-rides-ny-485508`.`zoomcamp`.`yellow_tripdata_2020_09`
+  UNION ALL
+  SELECT
+    unique_row_id
+  FROM
+    `taxi-rides-ny-485508`.`zoomcamp`.`yellow_tripdata_2020_10`
+  UNION ALL
+  SELECT
+    unique_row_id
+  FROM
+    `taxi-rides-ny-485508`.`zoomcamp`.`yellow_tripdata_2020_11`
+  UNION ALL
+  SELECT
+    unique_row_id
+  FROM
+    `taxi-rides-ny-485508`.`zoomcamp`.`yellow_tripdata_2020_12` );
